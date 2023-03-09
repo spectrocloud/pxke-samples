a90b54f495970bd53cd713267d7ead969cd0f46c
venkatnsrinivasan <venkat@spectrocloud.com>
2023-03-08 19:29:04 -0800

    Refactoring image build steps

--- build.sh
+++ build.sh
@@ -3,12 +3,37 @@
 source versions.sh
 
 set -ex
+ISO_IMAGE_NAME="${ISO_IMAGE_NAME:-spectro-installer}"
+KAIROS_VERSION="${KAIROS_VERSION:-v1.5.0}"
+SPECTRO_VERSION="${SPECTRO_VERSION:-v3.2.2-alpha6}"
+INSTALLER_IMAGE_NAME="${IMAGE_NAME:-gcr.io/spectro-dev-public/spectro-edge-installer}"
+INSTALLER_IMAGE="${INSTALLER_IMAGE_NAME}:${SPECTRO_VERSION}"
+USER_DATA_FILE="${USER_DATA_FILE:-user-data}"
+BUILD_PLATFORM="${BUILD_PLATFORM:-linux/amd64}"
+
+echo "Building custom $IMAGE from $BASE_IMAGE"
+
+if [ -f $USER_DATA_FILE ]; then
+ cp $USER_DATA_FILE overlay/files-iso/config.yaml
+fi
+docker build  --build-arg SPECTRO_VERSION=$SPECTRO_VERSION \
+              --build-arg BASE_IMAGE_TAG=$KAIROS_VERSION \
+               -t $INSTALLER_IMAGE -f images/Dockerfile.${FLAVOR} .
+
+
+echo "Building $ISO.iso from $IMAGE"
+docker run -v $PWD:/cOS \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+             -i --rm quay.io/kairos/osbuilder-tools:v0.3.3 --name $ISO_IMAGE_NAME \
+             --debug build-iso --date=false $INSTALLER_IMAGE  --local --overlay-iso /cOS/overlay/files-iso  --output /cOS/
+
+
 for k8s_version in ${K8S_VERSIONS//,/ }
 do 
 IMAGE=$IMAGE_REPO:$k8s_version
 docker build --build-arg K8S_VERSION=$k8s_version \
-             --build-arg BASE_REPO=$BASE_REPO \
-             --target ${BUILD_TARGET:-release} \
+             --build-arg BASE_IMAGE_TAG=$KAIROS_VERSION \
+             --build-arg SPECTRO_VERSION=$SPECTRO_VERSION \
              -t $IMAGE \
              -f images/Dockerfile.${FLAVOR} ./
 done
@@ -17,4 +42,4 @@ if [[ "$PUSH_BUILD" == "true" ]]; then
   echo "Pushing image"
   docker push "$IMAGE"
 fi
-
+rm -rf  overlay/files-iso/config.yaml
--- versions.sh
+++ versions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
 BASE_REPO=${BASE_REPO:-quay.io/kairos}
-K3S_VERSIONS="v1.25.2-k3s1,v1.24.6-k3s1,v1.23.12-k3s1,v1.22.15-k3s1"
+K3S_VERSIONS="1.25.2-k3s1,1.25.0-k3s1,1.24.6-k3s1,1.24.4-k3s1,1.23.12-k3s1,1.23.10-k3s1,1.22.15-k3s1,1.22.13-k3s1"
 RKE2_VERSIONS="v1.24.6-rke2r1,v1.23.12-rke2r1,v1.22.15-rke2r1,v1.25.2-rke2r1"
 MICROK8S_VERSIONS="1.25,1.24"
-FLAVOR="${FLAVOR:-ubuntu-rke2}"
+KUBEADM_VERSIONS="1.25.2,1.24.6,1.23.12,1.22.15"
+FLAVOR="${FLAVOR:-ubuntu-k3s}"
 
 getK8SVersions() {
-	IFS='- ' read -r -a strs <<< "$FLAVOR"
-	k8sDistro=${strs[1]}
+        k8sDistro=$(echo $FLAVOR | rev | cut -d "-" -f 1 | rev)
 
 	case $k8sDistro in
   
@@ -22,7 +22,9 @@ getK8SVersions() {
          microk8s)
 	     echo $MICROK8S_VERSIONS
              ;;
-
+         kubeadm)
+	     echo $KUBEADM_VERSIONS
+	     ;;
             *)
              echo -n "unknown"
              ;;
